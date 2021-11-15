e553a861b5c9b893f15f10245564c3eeca910fba
daffaraihanz <drzaki2002@gmail.com>
2021-11-15 04:24:34 -0800

    add screen home

--- app/src/main/java/com/kelompok2/tasku/LoginAct.java
+++ app/src/main/java/com/kelompok2/tasku/LoginAct.java
@@ -2,13 +2,31 @@ package com.kelompok2.tasku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginAct extends AppCompatActivity {
+
+    Button button_masuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button_masuk = findViewById(R.id.button_masuk);
+
+        button_masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toHome = new Intent(LoginAct.this, HomeAct.class);
+                startActivity(toHome);
+                finish();
+            }
+        });
     }
+
+
 }
